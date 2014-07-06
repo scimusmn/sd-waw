8ee8c101be25a9dc27f914b9e040aba61f007189
bryan kennedy <bkennedy@smm.org>
2014-07-06 18:10:16 -0500

    Removing rewind that's throwing errors

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,6 +1,10 @@
 Template.postsList.helpers({
   postsWithRank: function() {
-    this.posts.rewind();
+
+    // This is throwing errors. Not sure what it does,
+    // so I'm removing it for now
+    //this.posts.rewind();
+
     return this.posts.map(function(post, index, cursor) {
       post._rank = index;
       return post;
