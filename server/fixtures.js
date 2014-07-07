19a488bfbdaa8d3bce6e8980a1d5e38b368a2c22
bryan kennedy <bkennedy@smm.org>
2014-07-07 10:16:29 -0500

    Content update for Components
    Listing out all of the actual components that we're developing for the
    Where are We's

--- server/fixtures.js
+++ server/fixtures.js
@@ -27,15 +27,94 @@ if ( Meteor.users.find().count() === 0 ) {
  * Setup admin user
  */
 if (Components.find().count() === 0) {
-    for (var i = 0; i < 10; i++) {
-        Components.insert({
-            title: 'Test component #' + i,
-            author: admin.profile.name,
-            userId: admin._id,
-            submitted: now - i * 3600 * 1000 + 1,
-            bodyCopy: dimsum()
-        });
-    }
+    var torreyPines = Components.insert({
+        title: 'Torrey Pines',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'The Torrey Pine is a rare tree. This WAW explores its natural environment.',
+        componentNum: '0416',
+        bodyCopy: dimsum()
+    });
+
+    var wetlands = Components.insert({
+        title: 'Wetlands',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Wetlands general description.',
+        componentNum: '0316',
+        bodyCopy: dimsum()
+    });
+
+    var canyons = Components.insert({
+        title: 'Canyons',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Canyons general description.',
+        componentNum: '0516',
+        bodyCopy: dimsum()
+    });
+
+    var streams = Components.insert({
+        title: 'Streams',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Streams general description.',
+        componentNum: '0616',
+        bodyCopy: dimsum()
+    });
+
+    var chaparral = Components.insert({
+        title: 'Chaparral',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Chaparral general description.',
+        componentNum: '0704',
+        bodyCopy: dimsum()
+    });
+
+    var oakTree = Components.insert({
+        title: 'Oak Tree',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Oak Tree general description.',
+        componentNum: '0813',
+        bodyCopy: dimsum()
+    });
+
+    var mountains = Components.insert({
+        title: 'Mountains',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Mountains general description.',
+        componentNum: '0816',
+        bodyCopy: dimsum()
+    });
+
+    var desert = Components.insert({
+        title: 'Desert',
+        userId: admin._id,
+        author: admin.profile.name,
+        // TODO - Not sure if I need this field
+        submitted: now - i * 3600 * 1000 + 1,
+        description: 'Desert general description.',
+        componentNum: '0916',
+        bodyCopy: dimsum()
+    });
+
 }
 
 if (Posts.find().count() === 0) {
