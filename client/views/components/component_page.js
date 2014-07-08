06a521ec97918cbb385c23fa8b1c8d6b4b2de74c
bryan kennedy <bkennedy@smm.org>
2014-07-08 02:07:08 -0500

    Animate between different map stages

--- client/views/components/component_page.js
+++ client/views/components/component_page.js
@@ -1,3 +1,16 @@
+/**
+ * Show and hide the map layers
+ */
+Template.componentPage.events({
+    'click .container-map': function(e, template) {
+        if ($(e.target).hasClass('natural')) {
+            $(e.target).removeClass('natural');
+        } else {
+            $(e.target).addClass('natural');
+        }
+    }
+});
+
 Template.componentPage.helpers({
     dork: function() {
         return 'whort';
