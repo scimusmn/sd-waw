f13fcbf0633e6dbe246d11693d8f21f6ded10610
bryan kennedy <bkennedy@smm.org>
2014-07-17 16:25:12 -0500

    Removing comments

--- client/views/components/components_list.js
+++ client/views/components/components_list.js
@@ -1,29 +1,6 @@
 /**
  * Helpers for the Components List
  */
-
-//Template.componentsList.helpers({
-
-    ////this.posts.rewind();
-    ////return this.posts.map(function(post, index, cursor) {
-      ////post._rank = index;
-      ////return post;
-    ////});
-  ////}
-
-    //components: function() {
-        //this.components.rewind();
-        //Meteor.setTimeout(function(){
-            //console.log('components this - ', this);
-        //}, 5000);
-        //return this.components.map(function(components, index, cursor) {
-            ////post._rank = index;
-            //return components;
-        //});
-    //}
-
-//});
-
 Template.componentsList.rendered = function() {
     console.log('componentsList this - ', this);
 };
