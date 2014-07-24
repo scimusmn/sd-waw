c9fbbdad4b5b5e930d03e2c9e325e7468c5f3b7c
bryan kennedy <bkennedy@smm.org>
2014-07-24 13:00:28 -0500

    Describing some aspects of the component animation

--- client/views/components/component_item.js
+++ client/views/components/component_item.js
@@ -1,17 +1,20 @@
 /**
- * Helpers for the component item
+ * Helpers for the Component item
  */
 Template.componentItem.helpers({
+    /**
+     * Set component item styles to cascafe-fade in
+     */
     attributes: function() {
         order = this.order;
         var attributes = {};
         var delta = 50;
+        // Add an expanding delay to achieve cascade effect
         delay = ((order * 100) + 0);
         console.log(delay);
         attributes.style = [
             'transition-duration: 400ms;',
             'transition-timing-function: linear;',
-            //'transition-timing-function: cubic-bezier(1.000, 0.000, 0.000, 1.000);',
             'transition-delay: ' + delay + 'ms;'
         ];
         attributes.class = 'component loading'
@@ -20,11 +23,19 @@ Template.componentItem.helpers({
 });
 
 Template.componentItem.rendered = function() {
+    /**
+     * Start the animation once the page is rendered.
+     *
+     * We start the animation by removing the loading style. The loading style
+     * has opacity set to zero and the other styles will overrride this once
+     * loading is removed.
+     */
     var $item = $(this.find('.component'));
     Meteor.defer(function() {
         $item.removeClass('loading');
     });
 }
+
     //Meteor.setTimeout(function(){
         //console.log('componentItem this - ', this);
     //}, 5000);
