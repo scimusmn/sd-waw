a7db70040bb851eeaf0077dacac2f06d494e5d6b
bryan kennedy <bkennedy@smm.org>
2014-11-17 18:19:42 -0600

    Fixing the singlePage template to pull subpages
    This makes the subpages correctly pull all the pages with the same top
    ID

--- server/publications.js
+++ server/publications.js
@@ -21,6 +21,7 @@ Meteor.publish('singlePage', function(colorPalate, link) {
      * Return any page with the URL ID
      * as well as any subpages with the same parentId
      */
-    return Pages.find( { $and: [ { 'colorPalate': colorPalate, 'link': link } ] } );
-    //return id && Pages.find( { $or: [ {_id: id}, {parentId: id} ] } );
+    var topPage = Pages.findOne( { $and: [ { 'colorPalate': colorPalate, 'link': link } ] } );
+    var id = topPage._id;
+    return id && Pages.find( { $or: [ {_id: id}, {parentId: id} ] } );
 });
