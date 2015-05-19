73e646086ce5be86b9b14696b544d6666c345003
bryan kennedy <bkennedy@smm.org>
2015-05-19 10:39:53 -0500

    Adding settings flag for show/hide cursor

---
+++ client/main.js
@@ -0,0 +1,24 @@
+if (Meteor.isClient) {
+
+  /**
+   * Set body styles for showing or hiding the cursor based on settings
+   *
+   * We show the cursor in development for obvious reasons, but need to hide
+   * it when the system is in production, touch-screen.
+   */
+  if (Meteor.settings &&
+      Meteor.settings.public &&
+      Meteor.settings.public.cursor) {
+
+    Meteor.startup(function() {
+      console.log('body - ', $('body'));
+      $('body').addClass('show-cursor');
+    });
+
+  } else {
+    Meteor.startup(function() {
+      console.log('body - ', $('body'));
+      $('body').addClass('hide-cursor');
+    });
+  }
+}
