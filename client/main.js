79adaa57960e4bd39244eda5ca2365e9ff33b13a
bryan kennedy <bkennedy@smm.org>
2016-02-03 09:37:39 -0600

    Indentation cleanup for style

--- client/main.js
+++ client/main.js
@@ -1,24 +1,24 @@
 if (Meteor.isClient) {
 
   /**
-   * Set body styles for showing or hiding the cursor based on settings
-   *
-   * We show the cursor in development for obvious reasons, but need to hide
-   * it when the system is in production, touch-screen.
-   */
+  * Set body styles for showing or hiding the cursor based on settings
+  *
+  * We show the cursor in development for obvious reasons, but need to hide
+  * it when the system is in production, touch-screen.
+  */
   if (Meteor.settings &&
-      Meteor.settings.public &&
-      Meteor.settings.public.cursor) {
+    Meteor.settings.public &&
+    Meteor.settings.public.cursor) {
 
-    Meteor.startup(function() {
-      console.log('body - ', $('body'));
-      $('body').addClass('show-cursor');
-    });
+      Meteor.startup(function() {
+        console.log('body - ', $('body'));
+        $('body').addClass('show-cursor');
+      });
 
-  } else {
-    Meteor.startup(function() {
-      console.log('body - ', $('body'));
-      $('body').addClass('hide-cursor');
-    });
+    } else {
+      Meteor.startup(function() {
+        console.log('body - ', $('body'));
+        $('body').addClass('hide-cursor');
+      });
+    }
   }
-}
